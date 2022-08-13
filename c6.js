132e5c4b7e555331596cd65b2c7994287563a96b
sanad <109968451+oq7oq@users.noreply.github.com>
2022-08-13 14:03:31 +0300

    done

---
+++ c6.js
@@ -0,0 +1,54 @@
+alert(`مرحبا بك في موقعي الذي سيحسب لك درجتك`)
+
+
+let grade =prompt(`اكتب درجتك بالارقام`)
+
+console.log(grade)
+ 
+if (grade >= 90 && grade <=100 ) {
+
+console.log( `لقد حصلت على امتياز`)
+
+
+
+    
+}
+
+else if (grade >=80 && grade <=89) {
+    console.log("لقد حصلت على جيد جدا ")
+}
+
+else if (grade>=70 && grade <=79) {
+    console.log(`لقد حصلت على جيد `)
+}
+
+else if (grade>=60 && grade <=69) {
+    console.log(`لقد حصلت على مقبول`)
+}
+else if (grade>=50 && grade <=59) {
+    console.log(`لقد حصلت على ضعيف`)
+}
+
+else if (grade && grade <=50) {
+    console.log(`راسب`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// جافا سكربيت ليست جافا 
+
+
+
+
+
+
